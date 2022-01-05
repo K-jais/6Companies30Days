a7d5e0bdd780a90fec271d9e7de05a732f792089
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2022-01-05 11:24:10 +0530

    Create 10 max10.cpp

---
+++ GoldmanSachs/10 max10.cpp
@@ -0,0 +1,3 @@
+WE can use a priority queue(min heap) of 10 numbers and store first 10 numbers and then traverse the array from 11th number and then
+check if smaller number in min heap is smaller than cuurent number of array then eliminate that number in priority queue and add that
+current array element and if it is not small then move to the next element.
