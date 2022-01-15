da635263e3624afadd02e952538b78e37b16895e
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2022-01-15 19:20:35 +0530

    Create 3 rotate90.cpp

---
+++ Microsoft/3 rotate90.cpp
@@ -0,0 +1,12 @@
+void rotate(vector<vector<int> >& a)
+{
+    int n=a.size();
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            int temp=a[i][j];
+            a[i][j]=a[j][i];
+            a[j][i]=temp;
+        }
+    }
+    reverse(a.begin(),a.end());
+}
