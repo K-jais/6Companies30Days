0db3d87ad147ad6e18b9bb6da09ea48cddcbe60a
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2022-01-20 19:56:01 +0530

    Create 11 amend.cpp

---
+++ Adobe/11 amend.cpp
@@ -0,0 +1,12 @@
+string amendSentence (string s)
+    {
+        string res="";
+        for(int i=0;i<s.size();i++){
+            if(i!=0 && isupper(s[i]))
+            {
+                res+=" ";
+            }
+            res+=tolower(s[i]);
+        }
+        return res;
+    }
