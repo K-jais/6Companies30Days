b6620e631a303880257eff64b5f6de41f1d94138
Kush Jaiswal <75881393+K-jais@users.noreply.github.com>
2022-01-20 19:53:25 +0530

    Create 8 atoi.cpp

---
+++ Adobe/8 atoi.cpp
@@ -0,0 +1,20 @@
+int atoi(string str) {
+        int res=0;
+        int i=0,f=0;
+        int n=str.size();
+        if(str[i]=='-'){
+            f=1;
+            i++;
+        }
+        
+        while(i<n){
+            if(isdigit(str[i])){
+                res=res*10+(str[i]-'0');
+                i++;
+            }
+            else
+                return -1;
+        }
+        
+        return f==0? res:-res;
+    }
